dd831d4ebe51f91511e28b39104cf433e9a554f8
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-14 22:11:34 +0200

    move components into separate folders

--- client/components/App/index.jsx
+++ client/components/App/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Movies from './Movies';
+import Movies from '../Movies';
 
 const movies = [
   {
--- client/components/Movies/index.jsx
+++ client/components/Movies/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Movie from './Movie';
+import Movie from '../Movie';
 
 const Movies = props => (
   <ul className="movies">
