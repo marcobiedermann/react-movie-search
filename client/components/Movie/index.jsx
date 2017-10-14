22b464c6c23093fb72429371e0d1ec34d4c9b4a1
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-14 23:17:34 +0200

    add poster to movie

--- client/components/Movie/index.jsx
+++ client/components/Movie/index.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Movie = props => (
-  <h2>{props.title}</h2>
+  <div className="movie">
+    <figure className="movie__poster">
+      <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${props.poster_path}`} alt={props.title} />
+    </figure>
+    <h2 className="movie__title">{props.title}</h2>
+  </div>
 );
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
+  poster_path: PropTypes.string,
+};
+
+Movie.defaultProps = {
+  poster_path: '',
 };
 
 export default Movie;
