716297efa0cdcc4d838974e24185e3e5a72d5f07
Marco Biedermann <marco.biedermann@gmx.de>
2017-10-15 10:58:56 +0200

    add provider, store and root reducer

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components/App';
+import Root from './container/Root';
 
-render(<App />, document.getElementById('root'));
+render(<Root />, document.getElementById('root'));
