96cdc7de7f855d9de47a10a0cf9854921f797c5a
Marco Biedermann <marco.biedermann@gmx.de>
2018-11-11 21:36:59 +0100

    refactor: API
    Abstract API data in constants

--- client/assets/js/components/App/index.jsx
+++ client/assets/js/components/App/index.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-
+import { API_BASE, API_KEY } from '../../constants/themoviedb';
 import Movies from '../Movies';
 import Search from '../Search';
 import './app.css';
 
-const API_KEY = 'cfe422613b250f702980a3bbf9e90716';
-
 function getPopularMovies() {
   return new Promise((resolve, reject) => {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+    const url = `${API_BASE}/movie/popular?api_key=${API_KEY}`;
 
     fetch(url)
       .then(response => response.json())
@@ -21,7 +19,7 @@ function getPopularMovies() {
 
 function getMovies(query) {
   return new Promise((resolve, reject) => {
-    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`;
+    const url = `${API_BASE}/search/movie?query=${query}&api_key=${API_KEY}`;
 
     fetch(url)
       .then(response => response.json())
---
+++ client/assets/js/constants/themoviedb.js
@@ -0,0 +1,2 @@
+export const API_BASE = 'https://api.themoviedb.org/3';
+export const API_KEY = 'cfe422613b250f702980a3bbf9e90716';
