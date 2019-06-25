1e7adeb07030eaa0f6e41801453828db43b2745d
Marco Biedermannn <marco.biedermann@gmx.de>
2019-06-25 13:00:50 +0200

    feat: movie link
    add link to movie detail page

--- client/assets/js/components/Movie/index.jsx
+++ client/assets/js/components/Movie/index.jsx
@@ -1,25 +1,33 @@
 /* eslint-disable camelcase */
-import React from 'react';
 import PropTypes from 'prop-types';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as routes from '../../constants/routes';
 import './movie.css';
 
 const Movie = (props) => {
-  const { poster_path, title } = props;
+  const { id, poster_path, title } = props;
 
   return (
     <div className="movie">
       <figure className="movie__poster">
-        <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`} alt={title} />
+        <Link to={`${routes.MOVIES.path}/${id}`}>
+          <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`} alt={title} />
+        </Link>
       </figure>
       <div className="movie__body">
-        <h2 className="movie__title">{title}</h2>
+        <h2 className="movie__title">
+          <Link to={`${routes.MOVIES.path}/${id}`}>
+            {title}
+          </Link>
+        </h2>
       </div>
     </div>
   );
-}
+};
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   poster_path: PropTypes.string,
 };
