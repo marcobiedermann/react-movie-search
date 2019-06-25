03ed9de1a1729d6da64cc58e787b14ac33eb99e2
Marco Biedermannn <marco.biedermann@gmx.de>
2019-06-25 12:51:25 +0200

    feat: movie page
    add movie detail page

---
+++ client/assets/js/pages/Movie/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MoviePage = () => (
+  <div>
+    <h1>Movie</h1>
+  </div>
+);
+
+export default MoviePage;
--- client/assets/js/pages/Movies/index.jsx
+++ client/assets/js/pages/Movies/index.jsx
@@ -1,9 +1,34 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MoviePage from '../Movie';
 
-const MoviesPage = () => (
-  <div>
-    <h1>Movies</h1>
-  </div>
-);
+const MoviesPage = (props) => {
+  const { match } = props;
+
+  return (
+    <Switch>
+      <Route path={`${match.path}/:movieId`} component={MoviePage} />
+      <Route
+        path="/"
+        render={() => (
+          <div>
+            <h1>Movies</h1>
+          </div>
+        )}
+      />
+    </Switch>
+  );
+};
+
+MoviesPage.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string,
+  }),
+};
+
+MoviesPage.defaultProps = {
+  match: null,
+};
 
 export default MoviesPage;
