0ddee320b7ad0b97b21094be25e5ce4d78e827bc
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-12 20:10:52 +0100

    feat: Genres
    Fetch genres from API

--- client/assets/js/pages/Genres/index.tsx
+++ client/assets/js/pages/Genres/index.tsx
@@ -1,9 +1,41 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import useSWR from 'swr';
 
-const GenresPage: FC = () => (
-  <div>
-    <h1>Genres</h1>
-  </div>
-);
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface GenresPageData {
+  genres: Genre[];
+}
+
+const GenresPage: FC = () => {
+  const { data, error } = useSWR<GenresPageData>('/genre/movie/list');
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading …</div>;
+  }
+
+  const { genres } = data;
+
+  return (
+    <div>
+      <h1>Genres</h1>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>
+            <Link to={`/genres/${genre.id}`}>{genre.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default GenresPage;
